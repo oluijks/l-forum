ca8b54843c0ef2efc53630e5db91a1c7cf9cb8c1
Olaf Luijks <olafluijks@proton.me>
2024-11-23 16:41:59 +0100

    feat(db): update db config for use with turso

--- drizzle.config.sqlite.ts
+++ drizzle.config.sqlite.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from 'drizzle-kit';
 
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!process.env.TURSO_CONNECTION_URL)
+	throw new Error('TURSO_CONNECTION_URL is not set');
 
 export default defineConfig({
 	schema: './src/lib/server/db/sqlite/schema.ts',
 	out: './src/lib/server/db/sqlite/migrations',
 
 	dbCredentials: {
-		url: process.env.DATABASE_URL
+		url: process.env.TURSO_CONNECTION_URL,
+		authToken: process.env.TURSO_AUTH_TOKEN
 	},
 
 	verbose: true,
 	strict: true,
-	dialect: 'sqlite',
+	dialect: 'turso',
 	casing: 'snake_case'
 });
--- src/lib/server/db/sqlite/index.ts
+++ src/lib/server/db/sqlite/index.ts
@@ -1,13 +1,20 @@
 import { dev } from '$app/environment';
 import { env } from '$env/dynamic/private';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
+import { drizzle } from 'drizzle-orm/libsql';
 
 import * as schema from './schema.js';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+if (!env.TURSO_CONNECTION_URL)
+	throw new Error('TURSO_CONNECTION_URL is not set');
+if (!dev && !env.TURSO_AUTH_TOKEN)
+	throw new Error('DATABASE_AUTH_TOKEN is not set');
 
 export const db = drizzle({
-	connection: env.DATABASE_URL,
+	connection: {
+		url: env.TURSO_CONNECTION_URL,
+		authToken: env.TURSO_AUTH_TOKEN
+	},
+
 	schema,
 	casing: 'snake_case',
 	logger: dev
